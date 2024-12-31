38d3bd9e007cb5ca02e0179fea86f87ade191fb9
mhi59 <43959240+mhi59@users.noreply.github.com>
2024-12-31 11:40:36 +0100

    fix: fixing resources files (#8)
    Co-authored-by: Mehdi BENAISSA <mehdi.benaissa@getlinkgroup.com>

---
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Resources/Models/Order.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Resources;
+using System.Reflection;
+using System.Globalization;
+
+namespace P3.Resources.Models
+{
+    public static class Order
+    {
+        private static ResourceManager resourceManager = new ResourceManager("P3.Resources.Models.Order", Assembly.GetExecutingAssembly());
+        private static CultureInfo resourceCulture;
+
+        public static string ErrorMissingName
+        {
+            get
+            {
+                return resourceManager.GetString("ErrorMissingName", resourceCulture);
+            }
+        }
+        public static string ErrorMissingAddress
+        {
+            get
+            {
+                return resourceManager.GetString("ErrorMissingAddress", resourceCulture);
+            }
+        }
+        public static string ErrorMissingCity
+        {
+            get
+            {
+                return resourceManager.GetString("ErrorMissingCity", resourceCulture);
+            }
+        }
+        public static string ErrorMissingZipCode
+        {
+            get
+            {
+                return resourceManager.GetString("ErrorMissingZipCode", resourceCulture);
+            }
+        }
+        public static string ErrorMissingCountry
+        {
+            get
+            {
+                return resourceManager.GetString("ErrorMissingCountry", resourceCulture);
+            }
+        }
+    }
+}
+
---
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Resources/Models/Services/ProductServiceRessources.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Resources;
+using System.Reflection;
+using System.Globalization;
+
+namespace P3AddNewFunctionalityDotNetCore.Resources.Models.Services
+{
+    public static class ProductService
+    {
+        private static ResourceManager resourceManager = new ResourceManager("P3.Resources.Models.Services.ProductService", Assembly.GetExecutingAssembly());
+        private static CultureInfo resourceCulture;
+
+        public static string MissingName
+        {
+            get
+            {
+                return resourceManager.GetString("MissingName", resourceCulture);
+            }
+        }
+        public static string MissingPrice
+        {
+            get
+            {
+                return resourceManager.GetString("MissingPrice", resourceCulture);
+            }
+        }
+        public static string MissingStock
+        {
+            get
+            {
+                return resourceManager.GetString("MissingStock", resourceCulture);
+            }
+        }
+        public static string PriceNotANumber
+        {
+            get
+            {
+                return resourceManager.GetString("PriceNotANumber", resourceCulture);
+            }
+        }
+        public static string PriceNotGreaterThanZero
+        {
+            get
+            {
+                return resourceManager.GetString("PriceNotGreaterThanZero", resourceCulture);
+            }
+        }
+        public static string StockNotAnInteger
+        {
+            get
+            {
+                return resourceManager.GetString("StockNotAnInteger", resourceCulture);
+            }
+        }
+        public static string StockNotGreaterThanZero
+        {
+            get
+            {
+                return resourceManager.GetString("StockNotGreaterThanZero", resourceCulture);
+            }
+        }
+    }
+}
+
