ab70aaba4a01416c0a31ccb5b0f84ceed4d9b15e
Loïc BASTARAUD <39699697+ntxsay@users.noreply.github.com>
2025-03-11 13:33:17 -0400

    Ajout du premier test de création d'un produit

--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using P3AddNewFunctionalityDotNetCore.Models.ViewModels;
+using Xunit;
 
 namespace P3AddNewFunctionalityDotNetCore.Tests
 {
@@ -19,6 +20,42 @@ namespace P3AddNewFunctionalityDotNetCore.Tests
 
             // Assert
             Assert.Equal(1, 1);
+        }
+        
+        [Fact]
+        public void CreateProduct()
+        {
+            // Arrange 
+            
+            // Act
+            var product = new ProductViewModel()
+            {
+                Name = "Product 1",
+                Description = "Description of the product 1",
+                Details = "Details of the product 1",
+                Price = "1,20",
+                Stock = "20"
+            };
+
+            // Assert
+            
+            //S'assure que le nom du produit n'est pas null
+            Assert.NotNull(product.Name);
+            
+            //S'assure que le nom du produit n'est pas vide
+            Assert.NotEmpty(product.Name);
+            
+            //S'assure que la valeur du stock du produit est un nombre décimal ou entier
+            Assert.True(double.TryParse(product.Stock, out var stockValue));
+            
+            //S'assure que le stock est supérieur à 0
+            Assert.True(stockValue > 0);
+            
+            //S'assure que la valeur du prix du produit est un nombre décimal ou entier
+            Assert.True(double.TryParse(product.Stock, out var priceValue));
+            
+            //S'assure que le prix est supérieur à 0
+            Assert.True(priceValue > 0);
         }
 
         // TODO write test methods to ensure a correct coverage of all possibilities
