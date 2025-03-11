aaaaeebf4e5d4b324e6a0623d42d19120a075b51
BASTARAUD Loïc <ntxsay@hotmail.fr>
2025-03-11 10:36:49 -0400

    Ajout des attributs de validation

--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/ViewModels/ProductViewModel.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/ViewModels/ProductViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
 
 namespace P3AddNewFunctionalityDotNetCore.Models.ViewModels
 {
@@ -7,14 +8,22 @@ namespace P3AddNewFunctionalityDotNetCore.Models.ViewModels
         [BindNever]
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "ErrorMissingName")]
         public string Name { get; set; }
 
         public string Description { get; set; }
 
         public string Details { get; set; }
 
+        [Required(ErrorMessage = "ErrorMissingStock")]
+        [RegularExpression("^[0-9]+$", ErrorMessage = "StockNotAnInteger")]
+        [Range(1, int.MaxValue, ErrorMessage = "StockNotGreaterThanZero")]
         public string Stock { get; set; }
 
+
+        [Required(ErrorMessage = "ErrorMissingPrice")]
+        [DataType(DataType.Currency)]
+        [Range(0.01, double.MaxValue, ErrorMessage = "PriceNotGreaterThanZero")]
         public string Price { get; set; }
     }
 }
