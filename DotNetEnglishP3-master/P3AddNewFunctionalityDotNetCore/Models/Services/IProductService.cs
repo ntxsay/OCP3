091a157291f17f9b02c24a0ae186130cd26e79fe
Loïc BASTARAUD <39699697+ntxsay@users.noreply.github.com>
2025-03-28 05:43:56 -0400

    Suppression de la méthode "CheckProductModelErrors" comme indiquée dans le todo

--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/Services/IProductService.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/Services/IProductService.cs
@@ -14,7 +14,6 @@ namespace P3AddNewFunctionalityDotNetCore.Models.Services
         void UpdateProductQuantities();
         void SaveProduct(ProductViewModel product);
         void DeleteProduct(int id);
-        List<string> CheckProductModelErrors(ProductViewModel product);
         Task<Product> GetProduct(int id);
         Task<IList<Product>> GetProduct();
     }
--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/Services/ProductService.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/Services/ProductService.cs
@@ -89,49 +89,7 @@ namespace P3AddNewFunctionalityDotNetCore.Models.Services
                 _productRepository.UpdateProductStocks(line.Product.Id, line.Quantity);
             }
         }
-
-        // TODO this is an example method, remove it and perform model validation using data annotations
-        public List<string> CheckProductModelErrors(ProductViewModel product)
-        {
-            List<string> modelErrors = new List<string>();
-            if (product.Name == null || string.IsNullOrWhiteSpace(product.Name))
-            {
-                modelErrors.Add(_localizer["MissingName"]);
-            }
-
-            if (product.Price == null || string.IsNullOrWhiteSpace(product.Price))
-            {
-                modelErrors.Add(_localizer["MissingPrice"]);
-            }
-
-            if (!Double.TryParse(product.Price, out double pc))
-            {
-                modelErrors.Add(_localizer["PriceNotANumber"]);
-            }
-            else
-            {
-                if (pc <= 0)
-                    modelErrors.Add(_localizer["PriceNotGreaterThanZero"]);
-            }
-
-            if (product.Stock == null || string.IsNullOrWhiteSpace(product.Stock))
-            {
-                modelErrors.Add(_localizer["MissingQuantity"]);
-            }
-
-            if (!int.TryParse(product.Stock, out int qt))
-            {
-                modelErrors.Add(_localizer["StockNotAnInteger"]);
-            }
-            else
-            {
-                if (qt <= 0)
-                    modelErrors.Add(_localizer["StockNotGreaterThanZero"]);
-            }
-
-            return modelErrors;
-        }
-
+        
         public void SaveProduct(ProductViewModel product)
         {
             var productToAdd = MapToProductEntity(product);
