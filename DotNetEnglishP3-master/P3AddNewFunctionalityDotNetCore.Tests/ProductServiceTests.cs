abfbf70b63d67a1dc20012e4d0957dd4b56cea37
Loïc BASTARAUD <39699697+ntxsay@users.noreply.github.com>
2025-03-28 06:01:44 -0400

    Diviser le test complet en plusieurs petit tests (nom, stock, prix)

--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
@@ -6,24 +6,10 @@ namespace P3AddNewFunctionalityDotNetCore.Tests;
 public class ProductServiceTests
 {
     /// <summary>
-    /// Take this test method as a template to write your test method.
-    /// A test method must check if a definite method does its job:
-    /// returns an expected value from a particular set of parameters
+    /// L'objectif de ce test est de vérifier que le nom du produit est valide
     /// </summary>
     [Fact]
-    public void ExampleMethod()
-    {
-        // Arrange
-
-        // Act
-
-
-        // Assert
-        Assert.Equal(1, 1);
-    }
-        
-    [Fact]
-    public void CreateProduct()
+    public void CheckProductName()
     {
         // Arrange 
             
@@ -44,19 +30,68 @@ public class ProductServiceTests
             
         //S'assure que le nom du produit n'est pas vide
         Assert.NotEmpty(product.Name);
+        
+        //S'assure que le nom du produit est bien celle définie
+        Assert.Equal("Product 1", product.Name);
+    }
+    
+    /// <summary>
+    /// L'objectif de ce test est de vérifier que le stock du produit est valide
+    /// </summary>
+    [Fact]
+    public void CheckProductStock()
+    {
+        // Arrange 
             
+        // Act
+        var product = new ProductViewModel()
+        {
+            Name = "Product 1",
+            Description = "Description of the product 1",
+            Details = "Details of the product 1",
+            Price = "1,20",
+            Stock = "20"
+        };
+
+        // Assert
+        
         //S'assure que la valeur du stock du produit est un nombre décimal ou entier
         Assert.True(double.TryParse(product.Stock, out var stockValue));
             
         //S'assure que le stock est supérieur à 0
         Assert.True(stockValue > 0);
+        
+        //S'assure que la valeur du stock est bien celle définie
+        Assert.Equal(20, stockValue);
+    }
+    
+    /// <summary>
+    /// L'objectif de ce test est de vérifier que le prix du produit est valide
+    /// </summary>
+    [Fact]
+    public void CheckProductPrice()
+    {
+        // Arrange 
+            
+        // Act
+        var product = new ProductViewModel()
+        {
+            Name = "Product 1",
+            Description = "Description of the product 1",
+            Details = "Details of the product 1",
+            Price = "1,20",
+            Stock = "20"
+        };
+
+        // Assert
             
         //S'assure que la valeur du prix du produit est un nombre décimal ou entier
-        Assert.True(double.TryParse(product.Stock, out var priceValue));
+        Assert.True(double.TryParse(product.Price, out var priceValue));
             
         //S'assure que le prix est supérieur à 0
         Assert.True(priceValue > 0);
-    }
         
-    // TODO write test methods to ensure a correct coverage of all possibilities
+        //S'assure que la valeur du prix est bien celle définie
+        Assert.Equal(1.20, priceValue);
+    }
 }
